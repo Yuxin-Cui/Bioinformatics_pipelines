8e689ef2f9493b9dd216e41d7bc85fd5b283c645
Yuxin Cui <62621276+Yuxin-Cui@users.noreply.github.com>
2023-08-01 20:05:33 +0100

    Delete run_snakemake_pipeline.sh
    Delete an old bash script

--- run_snakemake_pipeline.sh
+++
@@ -1,7 +0,0 @@
-# Make sure the snakefile is validated using snakemake -n
-# conda activate snakemake
-snakemake --snakefile snakefile_bwa  -c 16
-eog plots/quals.svg
-snakemake --snakefile snakefile_bwa --rulegraph | dot -Tsvg > rulegraph.svg
-eog rulegraph.svg
- 
